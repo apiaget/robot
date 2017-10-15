5f86a1676798697b5b3500f32d1fddcfe96eda26
apiaget <axel.piaget@hotmail.com>
2017-10-15 16:06:14 +0000

    controle de plusieurs servos

--- robot/blueprints/robot.py
+++ robot/blueprints/robot.py
@@ -13,13 +13,21 @@ pwm = Adafruit_PCA9685.PCA9685()
 servo_1_status = False
 servo_1_position = 150
 
+SERVO_MAX = 4
+
+current_servo = 0
+
 servos = [[],[]]
-servos[0].append(False)
-servos[0].append(150)
-servos[0].append(1)
+servos[0].append(False) #le servo tourne ?
+servos[0].append(160)   #emplacement de la rotation
+servos[0].append(1)     #sens de rotation
+servos[0].append(160)   #minimum
+servos[0].append(660)   #maximum
 servos[1].append(False)
-servos[1].append(150)
+servos[1].append(165)
 servos[1].append(1)
+servos[1].append(160)
+servos[1].append(660)
 
 #servo_min = 150  # Min pulse length out of 4096
 #servo_max = 600  # Max pulse length out of 4096
@@ -39,14 +47,34 @@ def activate():
      global t1
      t1 = threading.Thread(target = move, args = ())
      t1.start()
-     global servos
-     servos[0][0] = True
-     rotation = request.json['rotation']
 
-     if(rotation == 1 or rotation == -1):
-          servos[0][2] = rotation
+#    global servos
+     #servos[0][0] = True
+
+     rotation = request.json['rotation']
+     servoid = request.json['id']
+
+     if(isinstance( servoid, int ) == False or servoid > SERVO_MAX or servoid <= 0):
+          dict = {"robot": "v1", "error": "Pas un nombre ou nombre trop grand !"}
+          resp = Response(json.dumps(dict), mimetype='application/json')
+          return resp
      else:
-          print("faux")
+          servoid = servoid - 1
+
+     if(isinstance( rotation, int) == False or (rotation  != 1 and rotation != -1)):
+          dict = {"robot": "v1", "error": "Sens de rotation non valide !"}
+          resp = Response(json.dumps(dict), mimetype='application/json')
+          return resp
+
+#     servos[0][2] = rotation
+     global servos
+     servos[servoid][0] = True
+     servos[servoid][2] = rotation
+
+     global current_servo
+     current_servo = servoid
+     print(servoid)
+     print(servos[servoid][1])
 
      resp = Response(json.dumps(request.json), mimetype='appliaction/json')
      return resp
@@ -55,16 +83,24 @@ def activate():
 @cross_origin()
 def stop():
      global servos
-     servos[0][0] = False
+     servos[current_servo][0] = False
 #     global servo_1_status
 #     servo_1_status = False
      resp = Response(json.dumps(request.json), mimetype='application/json')
      return resp
 
 def move():
-     print(servos[0][0])
-     while servos[0][0]:
-          print(servos[0][1])
-          servos[0][1] = servos[0][1] +  (6 * servos[0][2])
-          pwm.set_pwm(0, 0, servos[0][1])
-          time.sleep(0.1)
+     global servos
+     print("move")
+#     print(current_servo)
+#     print(servos[current_servo][1])
+     while servos[current_servo][0]:
+          print(servos[current_servo][1])
+          newvalue = servos[current_servo][1] + (5 * servos[current_servo][2])
+          if(newvalue >= servos[current_servo][3] and newvalue <= servos[current_servo][4]):
+               servos[current_servo][1] = newvalue
+               pwm.set_pwm(current_servo, 0, servos[current_servo][1])
+               time.sleep(0.1)
+          else:
+               time.sleep(0.1)
+
